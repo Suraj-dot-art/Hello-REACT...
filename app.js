2ce6f64fab66ac11f79870d07276760617a8f1e6
Suraj-dot-art <133409071+Suraj-dot-art@users.noreply.github.com>
2024-01-09 16:55:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const heading = React.createElement("h1", 
-{id: "headingOne"} ,  
-React.createElement("h3", {} , "This is my third Heading"));
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(heading);
+// This is how you create a element in core REACT
+const heading = React.createElement("h1", {id: "heading1"}, "NAMASTE from suraj");
 
-
-
-
-const root = ReactDOM.createRoot(document.querySelector("#firstDiv"));
-
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(heading);
+
+
+// This is how you create a element in jsx (makes readable and easy than core REACT)
+const jsxElement = <h2> HELLO FROM jsx element</h2>;
+root.render(jsxElement);
+
+// This is how you create a react functional componet in jsx
+
+const FunctionalComponent = () => <h1>Hello from functionalComponent</h1>;
+
+const Title = () => (<h1>
+    <FunctionalComponent/>
+    Hello from little stuart
+    </h1>);
+
+root.render(<Title/>);
+ 
